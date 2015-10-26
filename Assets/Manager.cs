ab8982597aaab5c532aaeb89d0626ed39885ee15
JPPinto <jose.vc.pinto@gmail.com>
2015-10-26 15:47:02 +0000

    Added Text element to canvas which detects the field; Added two new targets one for the field and another for the players.

---
+++ Assets/Manager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Manager : MonoBehaviour
+{
+
+    public Text displayedText = null;
+    public int playersFoundCounter = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+        displayedText = GetComponent<Text>();
+        displayedText.text = "Waiting for FIELD...";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject field = GameObject.FindGameObjectWithTag("Field");
+
+        if (field)
+        {
+            if (field.GetComponent<MeshRenderer>().isVisible)
+            {
+                if (playersFoundCounter == 0)
+                {
+                    displayedText.text = "";
+                }
+                displayedText.text = "FIELD found, Waiting for " + playersFoundCounter + " players...";
+            }
+            else
+                displayedText.text = "Waiting for FIELD...";
+
+        }
+
+    }
+}
