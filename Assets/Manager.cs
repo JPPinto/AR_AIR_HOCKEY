571064b9161cb5b59a2913394e86893b01276c56
Eduardo Fernandes <ei12130@fe.up.pt>
2015-11-07 00:13:52 +0000

    Reformat code

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -14,19 +14,20 @@ using System.Collections;
  */
 public enum State : int { STARTING, WAITING_PLAYERS, PLAYING, PAUSED, ENDED };
 
-public class Manager : MonoBehaviour
-{
+public class Manager : MonoBehaviour {
+    // State vars
     private State stateMachine = State.STARTING;
-    public Text displayedText = null;
     public int playersFoundCounter = 0;
     public int playerOneCounter = 0;
     public int playerTwoCounter = 0;
+
+    // Game objects
+    public Text displayedText = null;
     public GameObject scoreOne;
     public GameObject scoreTwo;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         playerOneCounter = 0;
         playerTwoCounter = 0;
         displayedText = GetComponent<Text>();
@@ -34,83 +35,71 @@ public class Manager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         UpdateState();
         UpdateText();
         UpdateScore();
     }
 
-    public State getCurrentState()
-    {
+    public State getCurrentState() {
         return stateMachine;
     }
 
     //TODO Missing End condition handling
-    void UpdateState()
-    {
+    void UpdateState() {
         //Incremented on every call
         playersFoundCounter = 0;
 
         GameObject field = GameObject.FindGameObjectWithTag("Field");
-        if (field)
-        {
-            if (field.GetComponent<MeshRenderer>().isVisible)
-            {
+        if (field) {
+            if (field.GetComponent<MeshRenderer>().isVisible) {
                 GameObject playerOne = GameObject.FindGameObjectWithTag("PlayerOne");
                 GameObject playerTwo = GameObject.FindGameObjectWithTag("PlayerTwo");
 
-                if (playerOne.GetComponent<MeshRenderer>().enabled)
-                {
+                if (playerOne.GetComponent<MeshRenderer>().enabled) {
                     playersFoundCounter++;
                 }
 
-                if (playerTwo.GetComponent<MeshRenderer>().enabled)
-                {
+                if (playerTwo.GetComponent<MeshRenderer>().enabled) {
                     playersFoundCounter++;
                 }
 
-                if (stateMachine.Equals(State.PLAYING) && playersFoundCounter < 2)
-                {
+                if (stateMachine.Equals(State.PLAYING) && playersFoundCounter < 2) {
                     stateMachine = State.PAUSED;
                     return;
                 }
 
-                if (playersFoundCounter == 2)
-                {
+                if (playersFoundCounter == 2) {
                     stateMachine = State.PLAYING;
-                }
-                else
-                {
+                } else {
                     if (!stateMachine.Equals(State.PAUSED))
                         stateMachine = State.WAITING_PLAYERS;
                 }
-            }
-            else { stateMachine = State.STARTING; }
+            } else { stateMachine = State.STARTING; }
         }
 
     }
 
-    void UpdateText()
-    {
-
-        switch (stateMachine)
-        {
-
+    void UpdateText() {
+        switch (stateMachine) {
             case State.STARTING:
                 playerOneCounter = 0;
                 playerTwoCounter = 0;
                 displayedText.text = "Waiting for field...";
                 break;
+
             case State.WAITING_PLAYERS:
                 displayedText.text = "Waiting for " + (2 - playersFoundCounter) + " players...";
                 break;
+
             case State.PLAYING:
                 displayedText.text = "GO!";
                 break;
+
             case State.PAUSED:
                 displayedText.text = "PAUSE\n " + (2 - playersFoundCounter) + " undetected";
                 break;
+
             case State.ENDED:
                 displayedText.text = "Waiting for FIELD...";
                 break;
